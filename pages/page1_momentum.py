525e8bd6bb143c540fb5e9a00f1aa10476a3e09e
Abraao Torquez <atorquez@hotmail.com>
2026-09-08 19:22:41 -0400

    change label page5

--- pages/page1_momentum.py
+++ pages/page1_momentum.py
@@ -997,9 +997,9 @@ min_price = st.number_input(
 
 max_price = st.number_input(
     "Maximum Price ($)",
-    value=500.0,
+    value=120.0,
     min_value=40.0,
-    max_value=500.0,
+    max_value=120.0,
     key="momentum_max_price"
 )
 
